8179af0db04cc63ca1f1810fe6246b79794890c6
Regis <dev05@sistemaeso.com.br>
2026-03-04 08:48:33 -0300

    Create a Cart Store and the Pinia Devtools Timeline

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 
 import { useProductStore } from "./stores/ProductStore";
+import { useCartStore } from "./stores/CartStore";
 
 const productStore = useProductStore();
+
+const cartStore = useCartStore();
 
 productStore.fill();
 </script>
@@ -17,6 +20,7 @@ productStore.fill();
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
