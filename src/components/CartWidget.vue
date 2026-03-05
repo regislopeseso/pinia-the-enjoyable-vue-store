6f535c6e58cd44d3aba1ad0b71551c4d9d5c343e
Regis <dev05@sistemaeso.com.br>
2026-03-05 08:46:53 -0300

    Reset the State in Pinia

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -31,7 +31,10 @@ const active = ref(false);
           Total: <strong>$40</strong>
         </div>
         <div class="flex justify-end">
-          <AppButton class="secondary mr-2">Clear Cart</AppButton>
+          <AppButton class="secondary mr-2" @click="cartStore.$reset()">
+            Clear Cart
+          </AppButton>
+
           <AppButton class="primary">Checkout</AppButton>
         </div>
       </div>
