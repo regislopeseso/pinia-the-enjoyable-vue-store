77a543dc076903daabafef410bdaa1db2be3b783
Regis <dev05@sistemaeso.com.br>
2026-03-06 09:01:39 -0300

    Preserve State with Hot Module Replacement

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from "pinia";
+import { defineStore, acceptHMRUpdate } from "pinia";
 import { groupBy } from "lodash";
 import { useAuthUserStore } from "./AuthUserStore";
 
@@ -60,3 +60,7 @@ export const useCartStore = defineStore("CartStore", {
     },
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot));
+}
