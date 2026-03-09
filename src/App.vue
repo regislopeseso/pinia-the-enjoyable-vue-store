ddef5a361b42ab2bd44f765f9cbb1b6f4628eaa3
Regis <dev05@sistemaeso.com.br>
2026-03-09 09:21:19 -0300

    Subscribe to the State (Part 2)

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,32 @@ const cartStore = useCartStore();
 
 const history = reactive([]);
 
+const future = reactive([]);
+
 const doingHistory = ref(false);
 
 history.push(JSON.stringify(cartStore.$state));
+
+const redo = () => {
+  const latestState = future.pop();
+
+  if (!latestState) return;
+
+  doingHistory.value = true;
+
+  history.push(latestState);
+
+  cartStore.$state = JSON.parse(latestState);
+
+  doingHistory.value = false;
+};
 
 const undo = () => {
   if (history.length === 1) return;
 
   doingHistory.value = true;
 
-  history.pop();
+  future.push(history.pop());
 
   cartStore.$state = JSON.parse(history.at(-1));
 
@@ -31,16 +47,18 @@ const undo = () => {
 cartStore.$subscribe((mutation, state) => {
   if (!doingHistory.value) {
     history.push(JSON.stringify(state));
+
+    future.splice(0, future.length);
   }
 });
 
-cartStore.$onAction(({ name, store, args, after, onError }) => {
-  if (name == "addItems") {
-    after(() => {
-      console.log("Teste: ", args[0]);
-    });
-  }
-});
+// cartStore.$onAction(({ name, store, args, after, onError }) => {
+//   if (name == "addItems") {
+//     after(() => {
+//       console.log("Teste: ", args[0]);
+//     });
+//   }
+// });
 
 productStore.fill();
 </script>
@@ -50,6 +68,7 @@ productStore.fill();
     <TheHeader />
     <div class="mb-5 flex justify-end">
       <AppButton @click="undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="redo">Redo</AppButton>
     </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
